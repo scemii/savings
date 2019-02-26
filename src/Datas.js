e143e96a5635bdbd1b3d74841467fec92343b47d
scemii <scemii@users.noreply.github.com>
2019-02-27 00:01:05 +0100

    adding input data
    adding firebase db control on the front

--- src/Datas.js
+++ src/Datas.js
@@ -11,7 +11,171 @@ class Datas extends Component {
       value: "",
       list: []
     };
+    this.mois = React.createRef();
   }
+
+  updateAmountQuery = event => {
+    this.setState({ value: event.target.value, list: [] });
+  };
+
+  handleKeyPress = event => {
+    if (event.key === "Enter" && this.mois.value === "Janvier") {
+      this.changeAmount();
+    } else {
+      if (event.key === "Enter" && this.mois.value === "Février") {
+        this.changeAmount2();
+      } else {
+        if (event.key === "Enter" && this.mois.value === "Mars") {
+          this.changeAmount3();
+        } else {
+          if (event.key === "Enter" && this.mois.value === "Avril") {
+            this.changeAmount4();
+          } else {
+            if (event.key === "Enter" && this.mois.value === "Mai") {
+              this.changeAmount5();
+            } else {
+              if (event.key === "Enter" && this.mois.value === "Juin") {
+                this.changeAmount6();
+              } else {
+                if (event.key === "Enter" && this.mois.value === "Juillet") {
+                  this.changeAmount7();
+                } else {
+                  if (event.key === "Enter" && this.mois.value === "Aout") {
+                    this.changeAmount8();
+                  } else {
+                    if (event.key === "Enter" && this.mois.value === "Septembre") {
+                      this.changeAmount9();
+                    } else {
+                      if (event.key === "Enter" && this.mois.value === "Octobre") {
+                        this.changeAmount10();
+                      } else {
+                        if (event.key === "Enter" && this.mois.value === "Novembre") {
+                          this.changeAmount11();
+                        } else {
+                          if (event.key === "Enter" && this.mois.value === "Décembre") {
+                            this.changeAmount12();
+                          } 
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+
+  changeAmount = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        janvier: Number(this.state.value)
+      });
+  };
+
+  changeAmount2 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        fevrier: Number(this.state.value)
+      });
+  };
+
+  changeAmount3 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        mars: Number(this.state.value)
+      });
+  };
+
+  changeAmount4 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        avril: Number(this.state.value)
+      });
+  };
+
+  changeAmount5 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        mai: Number(this.state.value)
+      });
+  };
+
+  changeAmount6 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        juin: Number(this.state.value)
+      });
+  };
+
+  changeAmount7 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        juillet: Number(this.state.value)
+      });
+  };
+
+  changeAmount8 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        aout: Number(this.state.value)
+      });
+  };
+
+  changeAmount9 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        septembre: Number(this.state.value)
+      });
+  };
+
+  changeAmount10 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        octobre: Number(this.state.value)
+      });
+  };
+
+  changeAmount11 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        novembre: Number(this.state.value)
+      });
+  };
+
+  changeAmount12 = () => {
+    firebase
+      .database()
+      .ref()
+      .update({
+        decembre: Number(this.state.value)
+      });
+  };
+
 
   componentDidMount() {
     const rootRef = firebase.database().ref();
@@ -141,7 +305,29 @@ class Datas extends Component {
   render() {
     return (
       (
-        <div> 
+        <div>
+          <h1 className="headerTitle"> Savings </h1>
+          <select className="select" ref={select => (this.mois = select)}>
+            <option>Janvier</option>
+            <option>Février</option>
+            <option>Mars</option>
+            <option>Avril</option>
+            <option>Mai</option>
+            <option>Juin</option>
+            <option>Juillet</option>
+            <option>Aout</option>
+            <option>Septembre</option>
+            <option>Octobre</option>
+            <option>Novembre</option>
+            <option>Décembre</option>
+          </select>
+          <input
+            className="input"
+            pattern="[0-9]"
+            onChange={this.updateAmountQuery}
+            onKeyPress={this.handleKeyPress}
+            placeholder="Montant"
+          /> 
           <Chart list={this.state.list} />
           <Chart2 list={this.state.list} />
           <Chart3 list={this.state.list} />
--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import fire from "./Fire";
 
 class Login extends Component {
